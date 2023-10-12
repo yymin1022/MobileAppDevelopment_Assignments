680a8a59ebf4d536b7ec02926bd418a65e3dfd3e
yymin1022 <yymin1022@gmail.com>
2023-10-12 09:21:04 +0900

    Update Assignment 4

--- assignment_4/test/widget_test.dart
+++ assignment_4/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:practice/main.dart';
+import 'package:assignment_4/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
