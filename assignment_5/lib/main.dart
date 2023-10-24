f267af798188b24aa634da2135e10ede14ebce59
yymin1022 <yymin1022@gmail.com>
2023-10-25 02:08:49 +0900

    Update Assignment 5

--- assignment_5/lib/main.dart
+++ assignment_5/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +7,183 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Grade Calculator",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<Student> _studentArr = [];
+  int _addPoint = -1;
+  int _point = 0;
+  String _name = "";
+
+  final _nameInputController = TextEditingController();
+  final _pointInputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Grade Calculator"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            )
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Information"),
+              Tab(text: "List")
+            ]),
+        ),
+        body: TabBarView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _nameInputController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Name"),
+                          onSaved: (name) => _name = name!,
+                          validator: (String? name) {
+                            if(name == null || name.isEmpty || name.contains(RegExp(r"\d"))){
+                              return "Please enter some string, not a number.";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _pointInputController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Project Point"),
+                          onSaved: (point) => _point = int.parse(point!),
+                          validator: (String? point) {
+                            if(point == null || point.isEmpty || point.contains(RegExp(r"\D"))){
+                              return "Please enter some integer.";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        DropdownButtonFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Choose the additional point"),
+                          items: List.generate(11, (idx) {
+                              if(idx == 0){
+                                return DropdownMenuItem(
+                                  value: idx - 1,
+                                  child: const Text("Choose the additional point"),
+                                );
+                              }
+                              return DropdownMenuItem(
+                                value: idx - 1,
+                                child: Text("${idx - 1} point")
+                              );
+                            }
+                          ),
+                          onChanged: (val){
+                            setState(() {
+                              _addPoint = val!;
+                            });
+                          },
+                          validator: (int? addPoint) {
+                            if(_addPoint == -1){
+                              return "Please select the point.";
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  GestureDetector(
+                    child: Container(
+                      color: Colors.blue,
+                      height: 75,
+                      width: double.infinity,
+                      child: const Center(child: Text("Enter")),
+                    ),
+                    onTap: (){
+                      if(_formKey.currentState!.validate()){
+                        setState(() {
+                          _formKey.currentState!.save();
+                          _studentArr.add(Student(_name, _point + _addPoint));
+                        });
+                      }
+                    },
+                  )
+                ]
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ReorderableListView.builder(
+              itemCount: _studentArr.length,
+              itemBuilder: (_, idx) {
+                return Dismissible(
+                  key: ValueKey(_studentArr[idx]),
+                  child: ListTile(
+                      leading: const Icon(Icons.home),
+                      title: Text("${_studentArr[idx].name}: ${_studentArr[idx].point}")
+                  ),
+                  onDismissed: (_){
+                    setState(() {
+                      _studentArr.removeAt(idx);
+                    });
+                  },
+                );
+              },
+              onReorder: (int oldIndex, int newIndex) {
+                setState(() {
+                  if(oldIndex < newIndex) {
+                    newIndex--;
+                  }
+                  var tmp = _studentArr[oldIndex];
+                  _studentArr[oldIndex] = _studentArr[newIndex];
+                  _studentArr[newIndex] = tmp;
+                });
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Student{
+  String name;
+  int point;
+  Student(var this.name, var this.point);
 }
